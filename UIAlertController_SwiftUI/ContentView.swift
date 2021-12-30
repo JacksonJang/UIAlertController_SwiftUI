934ed0dd7a57b2f146fc22d95e12ffb8af7f1fe5
장효원 <hyo961015@naver.com>
2021-12-30 16:48:32 +0900

    init

---
+++ UIAlertController_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIAlertController_SwiftUI
+//
+//  Created by 장효원 on 2021/12/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIAlertController_SwiftUI/UIAlertController_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIAlertController_SwiftUIApp.swift
+//  UIAlertController_SwiftUI
+//
+//  Created by 장효원 on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct UIAlertController_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
