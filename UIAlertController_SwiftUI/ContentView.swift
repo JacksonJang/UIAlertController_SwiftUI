c35bfd2e0f0cac87afefeaba0ab85f5aedda561f
장효원 <hyo961015@naver.com>
2021-12-30 21:15:29 +0900

    Complete Alert

--- UIAlertController_SwiftUI/ContentView.swift
+++ UIAlertController_SwiftUI/ContentView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var data: [String] = ["OK Alert", "OK And Cancel Alert", "ALL Alert", "OK ActionSheet", "OK And Cancel ActionSheet", "ALL ActionSheet"]
+    @State var data: [String] = ["OK Alert", "OK And Cancel Alert", "Destructive And Cancel"]
     
     var body: some View {
         VStack {
             List{
                 ForEach(0..<data.count){ number in
-                    CustomListCell(name: data[number])
+                    CustomListCell(name: data[number], index: number)
                 }
             }.listStyle(.plain)
         }
@@ -29,17 +29,50 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CustomListCell: View {
     @State private var showAlert = false
-    var name: String
+    var name:String
+    var index:Int
 
     var body: some View {
+        if index == 0 {
+            showOKAlert()
+        } else if index == 1 {
+            showOKAndCancelAlert()
+        } else if index == 2 {
+            showDestructiveAndCancel()
+        }
+    }
+    
+    func showOKAlert() -> some View {
         Button("\(name)") {
                 showAlert = true
-            }
-            .alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("Title"),
-                    message: Text("Message")
-                )
-            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Title"),
+                message: Text("Message")
+            )
+        }
+    }
+    
+    func showOKAndCancelAlert() -> some View {
+        Button("\(name)") {
+                showAlert = true
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Title"), message: Text("Message"), primaryButton: .default(Text("OK"), action: {
+                    
+            }), secondaryButton: .cancel())
+        }
+    }
+    
+    func showDestructiveAndCancel() -> some View {
+        Button("\(name)") {
+                showAlert = true
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Title"), message: Text("Message"), primaryButton: .destructive(Text("Destructive"), action: {
+                print("Destructive")
+            }), secondaryButton: .cancel())
+        }
     }
 }
