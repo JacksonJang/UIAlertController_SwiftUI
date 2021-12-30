ec21a552ec5fcc178766c2788020d0558d559742
장효원 <hyo961015@naver.com>
2021-12-30 20:40:36 +0900

    Create "List" and ListCell

--- UIAlertController_SwiftUI/ContentView.swift
+++ UIAlertController_SwiftUI/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var data: [String] = ["OK Alert", "OK And Cancel Alert", "ALL Alert", "OK ActionSheet", "OK And Cancel ActionSheet", "ALL ActionSheet"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            List{
+                ForEach(0..<data.count){ number in
+                    CustomListCell(name: data[number])
+                }
+            }.listStyle(.plain)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomListCell: View {
+    @State private var showAlert = false
+    var name: String
+
+    var body: some View {
+        Button("\(name)") {
+                showAlert = true
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Title"),
+                    message: Text("Message")
+                )
+            }
     }
 }
